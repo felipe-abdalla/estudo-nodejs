12899fa33099e1b77dbc4ec8ee80062a4b4a2c55
felipe.abdalla <felipe.abdalla@gmail.com>
2025-04-02 23:56:33 -0300

    nova funcao

--- funcoes.js
+++ funcoes.js
@@ -2,6 +2,11 @@ function resultado(req, res) {
     res.status(200).json({message:'Esta Funcionando', Variavel: req.query})
 }
 
+function novo() {
+    console.log(' faz alguma coisa')
+    return
+}
 module.exports = {
-    resultado
+    resultado,
+    novo
 }
